48dd5e876e29f239baa7e3a7d66c67988b2ce5a9
Nikhil Srivastava <nikhilsri8052@gmail.com>
2026-06-29 17:45:28 +0530

    Modify app.run to use environment variables for port
    Update Flask app to run on specified port and host.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 ﻿from flask import Flask, render_template, request, jsonify
 import pickle
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -40,4 +41,5 @@ def predict():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
